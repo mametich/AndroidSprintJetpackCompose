bb034af8b231ef3018a387e8e86e33d017422647
RustemAshir <mametich@rambler.ru>
2025-04-29 23:10:50 +0300

    RCA06-Theme

--- app/src/main/java/com/example/androidsprintjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidsprintjetpackcompose/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
