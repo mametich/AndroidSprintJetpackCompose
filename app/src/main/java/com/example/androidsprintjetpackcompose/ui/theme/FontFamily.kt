27979526401057cfb3ddbd7a355c7217284deca8
RustemAshir <mametich@rambler.ru>
2025-06-02 23:07:41 +0300

    RCA14-CategoriesScreen

--- app/src/main/java/com/example/androidsprintjetpackcompose/ui/theme/FontFamily.kt
+++ app/src/main/java/com/example/androidsprintjetpackcompose/ui/theme/FontFamily.kt
@@ -2,16 +2,17 @@ package com.example.androidsprintjetpackcompose.ui.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import com.example.androidsprintjetpackcompose.R
 
 val MontserratAlternates = FontFamily(
-    Font(R.font.montserrat_alternates_light),
-    Font(R.font.montserrat_alternates_medium),
-    Font(R.font.montserrat_alternates_regular),
-    Font(R.font.montserrat_alternates_semibold),
+    Font(R.font.montserrat_alternates_light, FontWeight.Light),
+    Font(R.font.montserrat_alternates_medium, FontWeight.Medium),
+    Font(R.font.montserrat_alternates_regular, FontWeight.Normal),
+    Font(R.font.montserrat_alternates_semibold, FontWeight.SemiBold),
 )
 
 val Montserrat = FontFamily(
-    Font(R.font.montserrat_medium),
-    Font(R.font.montserrat_regular)
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_regular,FontWeight.Normal)
 )
